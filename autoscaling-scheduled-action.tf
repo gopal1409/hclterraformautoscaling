9cb9d76974737844bed16fa23d83714dad2e260c
Gopal <Gopal1409@gmail.com>
2021-08-26 04:16:32 -0700

    terraform

--- autoscaling-scheduled-action.tf
+++ autoscaling-scheduled-action.tf
@@ -1,10 +1,10 @@
-resource "aws_autoscaling_schedule" "increase_capacity_9am" {
+/*resource "aws_autoscaling_schedule" "increase_capacity_9am" {
   scheduled_action_name = "increase-capacity-9am"
   min_size = 4
   max_size = 10
   desired_capacity = 8
   start_time = "2021-09-09T18:00:00Z"
-  recurrence = "00 09 * * *"
+  #recurrence = "00 09 * * *"
   autoscaling_group_name = module.autoscaling.autoscaling_group_id
 }
 resource "aws_autoscaling_schedule" "decrease_capacity_9pm" {
@@ -13,6 +13,6 @@ resource "aws_autoscaling_schedule" "decrease_capacity_9pm" {
   max_size = 10
   desired_capacity = 2
   start_time = "2021-09-09T18:00:00Z"
-  recurrence = "00 21 * * *"
+  #recurrence = "00 21 * * *"
   autoscaling_group_name = module.autoscaling.autoscaling_group_id
-}
+}*/
