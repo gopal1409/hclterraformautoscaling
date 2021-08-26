75388f9ca46d690427f0b9af3ebde10c683cd6c5
Gopal <Gopal1409@gmail.com>
2021-08-26 03:33:15 -0700

    autoscaling with sg

--- autoscaling-with-launchconfiguration.tf
+++ autoscaling-with-launchconfiguration.tf
@@ -1,76 +1,95 @@
-module "autoscaling_example_complete" {
-  source  = "terraform-aws-modules/autoscaling/aws//examples/complete"
+# Autoscaling with Launch Configuration - Both created at a time
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
   version = "4.1.0"
-  #autoscaling group
-  name = "${local.name}-myag1"
+
+  # Autoscaling group
+  name            = "${local.name}-myasg1"
   use_name_prefix = false
-  min_size = 2
-  max_size = 8
-  desired_capacity = 4
+
+  min_size                  = 2
+  max_size                  = 10
+  desired_capacity          = 2
+  #desired_capacity          = 3  # Changed for testing Instance Refresh as part of Step-10
   wait_for_capacity_timeout = 0
-  health_check_type = "EC2"
-  #vpc_zone_identifier = module.vpc.private_subnets
-  service_linked_role_arn = aws_iam_service_linked_role.autoscaling.arn 
-  target_group_arns = module.alb.target_group_arns
-  #ASG life cycle hooks
-  intial_lifecycle_hooks = [
-  {
-    name = "Startup Life Cycle Hook"
-    default_result = continue
-    heartbeat_timeout = 60
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-    notification_metadata = jsonencode({"hello"="world"})
-  },
-  {
-    name = "Termination Life Cycle Hook"
-    default_result = continue
-    heartbeat_timeout = 180
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATION"
-    notification_metadata = jsonencode({"goodbye"="world"})
-  }
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.vpc.private_subnets
+  service_linked_role_arn   = aws_iam_service_linked_role.autoscaling.arn
+  # Associate ALB with ASG
+  target_group_arns         = module.alb.target_group_arns
+
+  # ASG Lifecycle Hooks
+  initial_lifecycle_hooks = [
+    {
+      name                 = "ExampleStartupLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 60
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                 = "ExampleTerminationLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 180
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
   ]
-  #ASG Instance refresh
+
+  # ASG Instance Referesh
   instance_refresh = {
-   strategy = "Rolling"
-   preferences = {
-       min_healthy_percentage = 50
-   }
-   trigger = ["tag","desired_capacity"]
+    strategy = "Rolling"
+    preferences = {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag", "desired_capacity"] # Desired Capacity here added for demostrating the Instance Refresh scenario
   }
-  #ASG launch configuration
-  lc_name = "${local.name}-mylc1"
-  use_lc = true
+
+  # ASG Launch configuration
+  lc_name   = "${local.name}-mylc1"
+  use_lc    = true
   create_lc = true
 
-  image_id = data.aws_ami.amzlinux2.id
-  instance_type = var.instance_type
-  key_name = var.instance_keypair
-  user_data = file("${path.module/app1-install.sh}")
-  ebs_optimized = true
-  enable_monitoring = true 
-  security_groups = [module.private_sg.security_groups_id]
+  image_id          = data.aws_ami.amzlinux2.id
+  instance_type     = var.instance_type
+  key_name          = var.instance_keypair
+  user_data         = file("${path.module}/app1-install.sh")
+  ebs_optimized     = true
+  enable_monitoring = true
+
+  security_groups             = [module.private_sg.security_group_id]
   associate_public_ip_address = false
-  #we will create spot instance
-  spot_price = "0.016"
+
+  # Add Spot Instances, which creates Spot Requests to get instances at the price listed (Optional argument)
+  spot_price        = "0.014"
+  #spot_price        = "0.016" # Change for Instance Refresh test
 
   ebs_block_device = [
-      {
-          device_name = "/dev/xvdz"
-          delete_on_termination = true
-          encrypted = true
-          volume_type = "gp2"
-          volume_size = "20"
-      },
+    {
+      device_name           = "/dev/xvdz"
+      delete_on_termination = true
+      encrypted             = true
+      volume_type           = "gp2"
+      volume_size           = "20"
+    },
   ]
+
   root_block_device = [
     {
-          delete_on_termination = true
-          encrypted = true
-          volume_type = "gp2"
-          volume_size = "15"
-      },
-    
-]
+      delete_on_termination = true
+      encrypted             = true
+      volume_size           = "15"
+      volume_type           = "gp2"
+    },
+  ]
 
-tags = local.asg_tags
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "optional" # At production grade you can change to "required", for our example if is optional we can get the content in metadata.html
+    http_put_response_hop_limit = 32
+  }
+
+  tags        = local.asg_tags
 }
