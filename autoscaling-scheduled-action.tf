0a14d723127a9cf86fd65c0fbd2dcac7fcfcd092
Gopal <Gopal1409@gmail.com>
2021-08-26 04:02:36 -0700

    autoscaling with sg

--- autoscaling-scheduled-action.tf
+++ autoscaling-scheduled-action.tf
@@ -3,7 +3,7 @@ resource "aws_autoscaling_schedule" "increase_capacity_9am" {
   min_size = 4
   max_size = 10
   desired_capacity = 8
-  start_time = "2016-12-11T18:00:00Z"
+  start_time = "2021-09-09T18:00:00Z"
   #recurrence = "00 09 * * *"
   autoscaling_group_name = module.autoscaling.autoscaling_group_id
 }
@@ -12,7 +12,7 @@ resource "aws_autoscaling_schedule" "decrease_capacity_9pm" {
   min_size = 2
   max_size = 10
   desired_capacity = 2
-  start_time = "2016-12-11T18:00:00Z"
+  start_time = "2021-09-09T18:00:00Z"
   #recurrence = "00 21 * * *"
   autoscaling_group_name = module.autoscaling.autoscaling_group_id
 }
