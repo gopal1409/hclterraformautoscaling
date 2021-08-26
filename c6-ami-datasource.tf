a3903384f824087316427b21241f162c5ce22137
Gopal <Gopal1409@gmail.com>
2021-08-26 03:46:45 -0700

    autoscaling with sg

--- c6-ami-datasource.tf
+++ c6-ami-datasource.tf
@@ -1,15 +1,20 @@
-data "aws_ami" "amznlinux2" {
- most_recent = true
-owners = ["amazon"]
-
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
-
-
- filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+data "aws_ami" "amzlinux2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm-*-gp2" ]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
 }
